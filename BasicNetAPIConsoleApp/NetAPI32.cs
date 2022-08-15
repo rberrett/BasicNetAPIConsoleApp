fac665bfcc3d0ae2210fb717e8cf0aebbf130480
Russell Berrett <rberrett@microsoft.com>
2022-08-15 12:41:49 -0700

    Add project files.

---
+++ BasicNetAPIConsoleApp/NetAPI32.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicNetAPIConsoleApp
+{
+    internal class NetAPI32
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct FileInfo3
+        {
+            public int SessionID;
+            public int Permission;
+            public int NumLocks;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string PathName;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string UserName;
+        }
+
+        [DllImport("netapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern int NetFileEnum(
+            string serverName,
+            string basePath,
+            string userName,
+            int level,
+            ref IntPtr buffer,
+            // MAX_PREFERRED_LENGTH == -1
+            int prefMaxLength,
+            out int entriesRead,
+            out int totalEntries,
+            ref int resumeHandle
+            );
+
+        [DllImport("Netapi32.dll", SetLastError = true)]
+        public static extern int NetApiBufferFree(IntPtr Buffer);
+
+        public static List<FileInfo3> BuildNetFileEnumList(string server = null)
+        {
+            List<FileInfo3> list = new List<FileInfo3>();
+
+            int entriesRead;
+            int totalEntries;
+            int resumeHandle = 0;
+            IntPtr pBuffer = IntPtr.Zero;
+            int status = NetFileEnum(server, null, null, 3, ref pBuffer, -1, out entriesRead, out totalEntries, ref resumeHandle);
+
+            if (status == 0 && entriesRead > 0)
+            {
+                Type shareinfoType = typeof(FileInfo3);
+                int offset = Marshal.SizeOf(shareinfoType);
+
+                for (int i = 0, item = pBuffer.ToInt32(); i < entriesRead; i++, item += offset)
+                {
+                    IntPtr pItem = new IntPtr(item);
+
+                    FileInfo3 fileInfo3 = (FileInfo3)Marshal.PtrToStructure(pItem, shareinfoType);
+                    list.Add(fileInfo3);
+                }
+                NetApiBufferFree(pBuffer);
+            }
+            return list;
+        }
+
+    }
+}
---
+++ BasicNetAPIConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicNetAPIConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(@"C:\temp\FileAccess\leftopen.txt");
+
+            List<NetAPI32.FileInfo3> fileconnections = NetAPI32.BuildNetFileEnumList();
+            Console.WriteLine($"Num file connections = {fileconnections.Count}");
+            foreach (NetAPI32.FileInfo3 fileconnection in fileconnections)
+            {
+                int remoteUserPrimition = fileconnection.Permission;
+                string remoteUsername = fileconnection.UserName;
+                string pathName = fileconnection.PathName;
+                int shareID = fileconnection.SessionID;
+                Console.WriteLine($"FileInfo3:pathName = {pathName}");
+            }
+            Console.WriteLine("All done. Hit enter key to quit...");
+            Console.ReadLine();
+
+            sw.WriteLine("Blah blah blah.");
+            sw.Close();
+        }
+    }
+}
