cf3d40a69c0adc86f0cd9619ab5cd2c9a703e6a3
Russell Berrett <rberrett@microsoft.com>
2022-08-23 12:14:12 -0700

    change paradigm to remote access instead of local

--- BasicNetAPIConsoleApp/Program.cs
+++ BasicNetAPIConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BasicNetAPIConsoleApp
@@ -11,23 +12,21 @@ namespace BasicNetAPIConsoleApp
     {
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter(@"C:\temp\FileAccess\leftopen.txt");
-
-            List<NetAPI32.FILE_INFO_3> list = NetAPI32.GetNetFileEnumList();
-            Console.WriteLine($"Num file connections = {list.Count}");
-            foreach (NetAPI32.FILE_INFO_3 file in list)
+            do
             {
-                int id = file.fi3_id;
-                int perm = file.fi3_permission;
-                string pathname= file.fi3_pathname;
-                string username = file.fi3_username;
-                Console.WriteLine($"FILE_INFO_3:pathname = {pathname}");
-            }
-            Console.WriteLine("All done. Hit enter key to quit...");
-            Console.ReadLine();
+                List<NetAPI32.FILE_INFO_3> list = NetAPI32.GetNetFileEnumList();
+                //Console.WriteLine($"Num file connections = {list.Count}");
+                foreach (NetAPI32.FILE_INFO_3 file in list)
+                {
+                    int id = file.fi3_id;
+                    int perm = file.fi3_permission;
+                    string pathname = file.fi3_pathname;
+                    string username = file.fi3_username;
+                    //Console.WriteLine($"FILE_INFO_3:pathname = {pathname}");
+                }
+                Thread.Sleep(100);
+            } while (true);
 
-            sw.WriteLine("Blah blah blah.");
-            sw.Close();
         }
     }
 }
